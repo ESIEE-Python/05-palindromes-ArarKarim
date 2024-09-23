acd25bf0d19a8c458abd8897888495ef78b05d96
ArarKarim <karim.arar@edu.esiee.fr>
2024-09-23 12:30:47 +0000

    exercice Palindrome

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 #### Fonction secondaire
 
+"""
+main.py : Script principal
+"""
+
 
 def ispalindrome(p):
-
-    # votre code ici
-    
+    """
+    Vérifie si la chaîne donnée est un palindrome.
+    """
+    accent = str.maketrans( "àáâäèéêëìíîïòóôöùúûüÀÁÂÄÈÉÊËÌÍÎÏÒÓÔÖÙÚÛÜçÇ",
+        "aaaaeeeeiiiioooouuuuAAAAEEEEIIIIOOOOUUUUcC")
+    p = p.translate(accent)
+    p = ''.join(char.lower() for char in p if char.isalnum())
+    if p == p[::-1]:
+        return True
     return False
 
 #### Fonction principale
 
 
 def main():
+    """
+    Fonction principale pour tester la fonction ispalindrome avec différentes chaînes.
+    """
 
-    # vos appels à la fonction secondaire ici
-
+    print(ispalindrome("bob"))
+    print(ispalindrome("salut"))
+    print(ispalindrome("Tu l'as trop écrasé, César, ce port-salut "))
+    print(ispalindrome("Madame"))
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
-
 
 if __name__ == "__main__":
     main()
